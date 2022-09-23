74dfbc3745d4c3737cde8adce791a0192d6b52e9
Alexis Skodak <skodak95alexis@gmail.com>
2022-09-23 10:55:30 +0200

    Update: project config

--- futuresqc_manager/settings.py
+++ futuresqc_manager/settings.py
@@ -76,6 +76,11 @@ CORS_ALLOW_HEADERS = default_headers + ("cache-control",)
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.FormParser",
+        "rest_framework.parsers.MultiPartParser",
+        "rest_framework.parsers.JSONParser",
+    ],
 }
 
 SITE_ID = 1
